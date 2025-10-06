5acf2b7535ab1397c2f01634d212d11e8d605fa9
hai8032 <haihph50907@gmail.com>
2025-10-07 02:15:37 +0700

    banner

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,6 +1,7 @@
 <?php 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PhimController;
+use App\Http\Controllers\BannerController;
 
 Route::get('/phim', [PhimController::class, 'index']);
 Route::get('/phim/{id}', [PhimController::class, 'show']);
@@ -8,3 +9,11 @@ Route::post('/phim', [PhimController::class, 'store']);
 Route::put('/phim/{id}', [PhimController::class, 'update']);
 Route::delete('/phim/{id}', [PhimController::class, 'destroy']);
 Route::apiResource('vai_tro', PhimController::class);
+
+
+
+Route::get('/banners', [BannerController::class, 'index']);
+Route::post('/banners', [BannerController::class, 'store']);
+Route::get('/banners/{id}', [BannerController::class, 'show']);
+Route::put('/banners/{id}', [BannerController::class, 'update']);
+Route::delete('/banners/{id}', [BannerController::class, 'destroy']);
