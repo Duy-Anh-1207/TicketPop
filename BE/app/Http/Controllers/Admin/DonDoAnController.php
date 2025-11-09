22bfff3e91210849197cea583cdaa1babba00429
duydat-cyber <duydat30052004@gmail.com>
2025-11-09 10:38:58 +0700

    add list donDoAn

--- BE/app/Http/Controllers/Admin/DonDoAnController.php
+++ BE/app/Http/Controllers/Admin/DonDoAnController.php
@@ -10,5 +10,34 @@ use Illuminate\Http\Request;
 
 class DonDoAnController extends Controller
 {
-    
+    public function index()
+    {
+        $data = DonDoAn::with([
+            'datVe.nguoiDung:id,ten,email',
+            'doAn:id,ten_do_an,image',
+        ])->latest()->get();
+        return response()->json([
+            'message' => 'Danh Sách Đơn Đồ Ăn',
+            'data' => $data
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'dat_ve_id' => 'required|exists:dat_ve,id',
+            'do_an_id' => 'required|exists:do_an,id',
+            'so_luong' => 'required|integer|min:1'
+        ]);
+        $doAn = DoAn::findOrFail($request->do_an_id);
+        $donDoAn = DonDoAn::create([
+            'dat_ve_id' => $request->dat_ve_id,
+            'do_an_id' => $request->do_an_id,
+            'gia_ban' => $doAn->gia_ban,
+            'so_luong' => $request->so_luong,
+        ]);
+        return response()->json([
+            'message' => 'Tạo đơn đồ ăn thành công',
+            'data' => $donDoAn->load('doAn','datVe')
+        ],201);
+    }
 }
